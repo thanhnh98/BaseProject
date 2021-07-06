1dc2f2722d09de619198f8c156f1cb9f8e960914
Thanh <thanh150898@gmail.com>
2021-07-06 18:04:53 +0700

    add motion layout

--- app/src/main/java/com/thanh_nguyen/baseproject/screens/motion_layout/MotionActivity.kt
+++ app/src/main/java/com/thanh_nguyen/baseproject/screens/motion_layout/MotionActivity.kt
@@ -1,9 +1,22 @@
 package com.thanh_nguyen.baseproject.screens.motion_layout
 
+import android.os.Bundle
+import android.util.Log
 import com.thanh_nguyen.baseproject.R
 import com.thanh_nguyen.baseproject.common.base.mvvm.activity.BaseActivity
 import com.thanh_nguyen.baseproject.databinding.ActivityMotionBinding
+import com.thanh_nguyen.baseproject.onClick
 
 class MotionActivity: BaseActivity<ActivityMotionBinding>() {
     override fun inflateLayout(): Int = R.layout.activity_motion
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.constraintLayout.onClick {
+            Log.e("click","motion clicked")
+        }
+    }
+
+    override fun onBackPressed() {
+        binding.constraintLayout.transitionToStart()
+    }
 }
